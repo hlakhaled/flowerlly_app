f9621223c8e40bd7a8f7e2ac78e1d42f8054c67b
hlakhaled <hlakhaled647@gmail.com>
2025-01-22 21:38:19 +0200

    add text style

---
+++ lib/constants/assets.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesBackground
+  /// assets/images/background.svg
+  static const String assetsImagesBackground = "assets/images/background.svg";
+}
+
