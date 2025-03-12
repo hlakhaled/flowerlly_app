d6315e9f65573e3613b82c784aeeecaf86d291a5
HlaKhaled <hlakhaled647@gmail.com>
2025-03-12 20:16:11 +0200

    generate plant entity adapter

--- lib/features/home/domain/entities/plant_entity.dart
+++ lib/features/home/domain/entities/plant_entity.dart
@@ -1,6 +1,12 @@
+import 'package:hive/hive.dart';
+  part 'plant_entity.g.dart';
+@HiveType(typeId: 0)
 class PlantEntity {
+@HiveField(0)
   final String image;
+@HiveField(1)
   final String title;
+  @HiveField(2)
   final int pId;
   PlantEntity({required this.image, required this.title,required this.pId});
 }
---
+++ lib/features/home/domain/entities/plant_entity.g.dart
@@ -0,0 +1,47 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'plant_entity.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class PlantEntityAdapter extends TypeAdapter<PlantEntity> {
+  @override
+  final int typeId = 0;
+
+  @override
+  PlantEntity read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return PlantEntity(
+      image: fields[0] as String,
+      title: fields[1] as String,
+      pId: fields[2] as int,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, PlantEntity obj) {
+    writer
+      ..writeByte(3)
+      ..writeByte(0)
+      ..write(obj.image)
+      ..writeByte(1)
+      ..write(obj.title)
+      ..writeByte(2)
+      ..write(obj.pId);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is PlantEntityAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
